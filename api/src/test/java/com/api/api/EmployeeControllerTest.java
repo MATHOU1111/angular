890c0df8e845474f5b385debce7214869c332916
mathou <mathisdumage0@gmail.com>
2024-05-16 16:16:15 +0200

    ajout spring boot

---
+++ api/src/test/java/com/api/api/EmployeeControllerTest.java
@@ -0,0 +1,42 @@
+package com.api.api;
+
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.api.api.service.EmployeeService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class EmployeeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private EmployeeService employeeService;
+
+
+    @Test
+    public void testGetEmployeesAll() throws Exception {
+        mockMvc.perform(get("/employees"))
+                .andExpect(status().isOk());
+    }
+    @Test
+    public void testGetEmployees() throws Exception {
+        mockMvc.perform(get("/employees"))
+                .andExpect(status().isOk());
+    }
+
+}
